4d3b6773576b3128b676d608047119e67b496fb0
grzpan83 <grzpan@gmail.com>
2018-04-10 01:01:30 +0200

    adding forms to the main page and some auth stuff tests

--- taskbase/tasklist/views.py
+++ taskbase/tasklist/views.py
@@ -1,9 +1,14 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import authenticate
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Task
 from .serializers import TaskSerializer
+from .forms import CreateTaskForm, UpdateTaskForm
 
 
 def sample_view_1(request):
@@ -31,14 +36,44 @@ def sample_view_2(request):
 
 class HomePageView(View):
     def get(self, request):
-        ctx = {'page_title': 'Tasks page', }
+        create_form = CreateTaskForm()
+        update_form = UpdateTaskForm()
+        ctx = {'page_title': 'Tasks page', 'create_form': create_form, 'update_form': update_form, }
+        # print(request.method)
+        # u = authenticate(username='user1', password='user1pass')
+        # print(type(u))
+        # print(u.is_authenticated)
+        # print(request.user)
+        # print(request.user.is_authenticated)
+        # print(request.user.id)
         return render(request, 'index.html', ctx)
 
 
-# retrieve all tasks (using GET) or create a new task (using POST)
 class TasksView(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+
+# retrieve all tasks (using GET) or create a new task (using POST)
+# class TasksView(LoginRequiredMixin, generics.ListCreateAPIView):
+#     def get_queryset(self):
+#         return Task.objects.filter(creator_id=self.request.user.id)
+#     serializer_class = TaskSerializer
+
+
+# class TasksView(APIView):
+#
+#     def get(self, request):
+#         tasks = Task.objects.all()
+#         serializer = TaskSerializer(tasks, many=True, context={'request': request, })
+#         return Response(serializer.data)
+#
+#     def post(self, request):
+#         serializer = TaskSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors)
 
 
 # retrieve (using GET), update (using PUT) or remove (using DELETE) a task with a given id
